aee338f7b6a4b91d7262aed5efaa07f89ad210b3
Brian Yang <brianjsyang@gmail.com>
2026-04-14 22:19:39 -0700

    Create clock object. Limit FPS to 60

--- main.py
+++ main.py
@@ -7,20 +7,23 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pyclock = pygame.time.Clock()
+    dt = 0
 
     # Game Loop
     while True:
         log_state()
+
         for event in pygame.event.get():
             # processing pygame event queue
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = pyclock.tick(60) / 1000
 
 
 if __name__ == "__main__":
