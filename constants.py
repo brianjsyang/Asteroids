c3ca9636654aa62d03940962ca2e64772a9eda34
Brian Yang <brianjsyang@gmail.com>
2026-04-16 22:32:22 -0700

    Print player and add movements using keyboard strokes

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+LINE_WIDTH = 2  # Width of the lines that draw the player
+
+PLAYER_RADIUS = 20  # Player ship radius
+PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pyclock = pygame.time.Clock()
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)  # type: ignore Verified working
+
+    # Player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     # Game Loop
@@ -19,7 +30,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
